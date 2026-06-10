ac412bb3b931a2a01b05ab297af8e3042f297ba0
sunnybunny24109 <161111420+sunnybunny24109@users.noreply.github.com>
2026-06-11 00:06:58 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggy-bucket-terraform-0099demo-s"
+  bucket = "swiggy-bucket-terraform-24109-demo-s"
 
   # Enable versioning
   versioning {
