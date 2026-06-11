88bf8d2da738df24ca7a0d2b50d0304d6c51f5b6
sunnybunny24109 <161111420+sunnybunny24109@users.noreply.github.com>
2026-06-11 17:29:29 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -20,9 +20,9 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 # Create a launch template for the EC2 instances
 resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
-  image_id      = "ami-07a00cf47dbbc844c"
+  image_id      = "ami-0db56f446d44f2f09"
   instance_type = "t3.micro"
-  key_name      = "kubeadmm"
+  key_name      = "sunny"
 
   network_interfaces {
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg-app.id]
